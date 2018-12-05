e8dc62ee73fb91c8fd617a2db3392616820ac5a0
FUJI Goro (gfx) <gfuji@cpan.org>
2018-12-06 01:07:31 +0900

    implement Mutation.createArticle

--- app/graphql/mutations/create_article.rb
+++ app/graphql/mutations/create_article.rb
@@ -1,14 +1,15 @@
 module Mutations
   class CreateArticle < GraphQL::Schema::RelayClassicMutation
-    # TODO: define return fields
-    # field :post, Types::PostType, null: false
 
-    # TODO: define arguments
-    # argument :name, String, required: true
+    argument :name, String, required: true
+    argument :content, String, required: true
 
-    # TODO: define resolve method
-    # def resolve(name:)
-    #   { post: ... }
-    # end
+    field :article, Types::ArticleType, null: false
+
+    def resolve(name:, content:)
+      article = Article.new(name: name, content: content)
+      article.save!
+      { article: article }
+    end
   end
 end
